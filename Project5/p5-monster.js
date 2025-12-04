f830c226150dd6c3580b6007838001b70e6624be
drewlesh <drewlesh24@gmail.com>
2025-12-03 17:39:41 -0800

    Remove old/un-organized files

--- Project5/p5-monster.js
+++
@@ -1,7 +0,0 @@
-module.exports = class Monster {
-    constructor({ monsterName = "Unknown", minimumLife = 0, currentLife = 100 }) {
-        this.monsterName = monsterName;
-        this.minimumLife = minmumLife
-        this.currentLife = currentLife;
-        this.isAlive = currentLife >= minmumLife;
-}}

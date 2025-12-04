0ef72e9bdb97681d57bd488e6b5d0bc3d7373ff8
drewlesh <drewlesh24@gmail.com>
2025-12-03 17:32:13 -0800

    Restor deleted files

---
+++ Lab3/README.md
@@ -0,0 +1,2 @@
+# CIT281-lab-3
+### [CIT Minor HOMEPAGE](https://drewlesh.github.io/)
---
+++ Project1/p1-date.js
@@ -0,0 +1,22 @@
+<<<<<<< HEAD
+/*
+
+CIT 281 Project 1
+Name: Drew Lesh
+
+*/
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+let indexOfDay = new Date().getDay();
+=======
+/*
+
+CIT 281 Project 1
+Name: Drew Lesh
+
+*/
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+let indexOfDay = new Date().getDay();
+>>>>>>> afef7c11d7cdb091cdfe841649d1d15acac5fb72
+console.log(days[indexOfDay]);
